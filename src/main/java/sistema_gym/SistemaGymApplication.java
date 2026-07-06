632fa40437e157c1af9a7221b03d3f5e1ac776dd
Pilar Russo <russo.pily@gmail.com>
2026-07-06 18:41:07 -0300

    Proyecto inicial

---
+++ src/main/java/sistema_gym/SistemaGymApplication.java
@@ -0,0 +1,13 @@
+package sistema_gym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaGymApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sistema_gym/SistemaGymApplicationTests.java
@@ -0,0 +1,13 @@
+package sistema_gym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaGymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
